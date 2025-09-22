5a428718dd9537a75a15f2a1cdf061d5ffadebf2
Joshua White <joshuawhite706@gmail.com>
2025-09-21 20:48:53 -0400

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b): 
     return a + b
 def subtract(a, b): 
@@ -5,4 +7,8 @@ def subtract(a, b):
 def multiply(a, b): 
     return a * b
 def divide(a, b): 
-    return a / b 
+    return a / b if b != 0 else "You cant Divide by zero"
+def power(a, b):
+    return a ** b
+def square_root(a):
+    return math.sqrt(a) if a > 0 else "Square root is negative"
